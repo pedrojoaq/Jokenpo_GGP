00038c533df1f6395b663868579ca3a74725f9dc
Pedro Joaquim <pedrojoaquimfl@gmail.com>
2026-04-20 12:58:14 -0300

    humanoXcomputador v0.4

--- humanoXcomputador.py
+++ humanoXcomputador.py
@@ -3,6 +3,10 @@ import time
 infinito = 1
 placarHumano = 0
 placarMaquina = 0
+placarJogadorUm = 0
+placarJogadorDois = 0
+placarComputadorUm = 0
+placarComputadorDois = 0
 
 maoTres = '''
   ____    ___
@@ -128,8 +132,156 @@ while escolhaModalidade < 1 or escolhaModalidade > 3:
         escolha = int(input("Digite novamente sua escolha: "))
 
 if(escolhaModalidade == 1):
-    print("Nada ainda...")
+    while infinito != 0:
+        print(divisao)
+        print("ESCOLHA SUA JOGADA - JOGADOR 1")
+        print(" ")
+        print("Pedra - 1")
+        print(" ")
+        print("Papel - 2")
+        print(" ")
+        print("Tesoura - 3")
+        print(" ")
+        escolhaJogadorUm = int(input("Digite sua escolha: "))
 
+        while escolhaJogadorUm < 1 or escolhaJogadorUm > 3:
+            print(" ")
+            print("Escolha um numero entre 1 e 3!")
+            print(" ")
+            escolhaJogadorUm = int(input("Digite novamente sua escolha: "))
+        
+        print(divisao)
+
+        print("ESCOLHA SUA JOGADA - JOGADOR 2")
+        print(" ")
+        print("Pedra - 1")
+        print(" ")
+        print("Papel - 2")
+        print(" ")
+        print("Tesoura - 3")
+        print(" ")
+        escolhaJogadorDois = int(input("Digite sua escolha: "))
+
+        while escolhaJogadorDois < 1 or escolhaJogadorDois > 3:
+            print(" ")
+            print("Escolha um numero entre 1 e 3!")
+            print(" ")
+            escolhaJogadorDois = int(input("Digite novamente sua escolha: "))
+
+        print(divisao)
+        time.sleep(1)
+        print(" ")
+        print(maoTres)
+        time.sleep(1)
+        print(" ")
+        print(maoDois)
+        time.sleep(1)
+        print(" ")
+        print(maoUm)
+        time.sleep(1)
+        print(" ")
+        print(ja)
+        time.sleep(1)
+        print(" ")
+        print(divisao)
+
+        if(escolhaJogadorUm == escolhaJogadorDois):
+            print("Empate!")
+
+        elif(escolhaJogadorUm == 1):
+            print("ESCOLHA JOGADOR UM:")
+            time.sleep(1)
+            print(pedra)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA JOGADOR DOIS:")
+            time.sleep(1)
+            if(escolhaJogadorDois == 2):
+                print(papel)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR DOIS venceu!")
+                placarJogadorDois+= 1
+            elif(escolhaJogadorDois == 3):
+                print(tesoura)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR UM venceu!")
+                placarJogadorUm+= 1
+
+        elif(escolhaJogadorUm == 2):
+            print("ESCOLHA JOGADOR UM:")
+            time.sleep(1)
+            print(papel)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA JOGADOR DOIS:")
+            time.sleep(1)
+            if(escolhaJogadorDois == 1):
+                print(pedra)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR UM venceu!")
+                placarJogadorUm+= 1
+            elif(escolhaJogadorDois == 3):
+                print(tesoura)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR DOIS venceu!")
+                placarJogadorDois+= 1
+
+        elif(escolhaJogadorUm == 3):
+            print("ESCOLHA JOGADOR UM:")
+            time.sleep(1)
+            print(tesoura)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA JOGADOR DOIS:")
+            time.sleep(1)
+            if(escolhaJogadorDois == 1):
+                print(pedra)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR DOIS venceu!")
+                placarJogadorDois+= 1
+            elif(escolhaJogadorDois == 2):
+                print(papel)
+                time.sleep(2)
+                print(divisao)
+                print("JOGADOR UM venceu!")
+                placarJogadorUm+= 1
+        
+        print(divisao)
+        print("Continuar - 1")
+        print(" ")
+        print("Sair - 0")
+        print(" ")
+        escolhaContinuar = int(input("Sua escolha: "))
+
+        while escolhaContinuar < 0 or escolhaContinuar > 1:
+          print(" ")
+          print("Escolha um número entre 0 e 1!")
+          print(" ")
+          escolha = int(input("Digite novamente sua escolha: "))
+        
+        if(escolhaContinuar == 0):
+            print(divisao)
+            time.sleep(1)
+            print(placar)
+            time.sleep(1)
+            print(" ")
+            print(f"JOGADOR UM - {placarJogadorUm} X JOGADOR DOIS - {placarJogadorDois}")
+            print(" ")
+            infinito = 0
 elif(escolhaModalidade == 2):
     while infinito != 0:
         print(divisao)
@@ -266,4 +418,154 @@ elif(escolhaModalidade == 2):
             print(" ")
             infinito = 0
 elif(escolhaModalidade == 3):
-    print("Nada ainda...")
+    while infinito != 0:
+        print(divisao)
+        print("COMPUTADOR 1 ESTÁ ESCOLHENDO UM ENTRE ESSES")
+        print(" ")
+        time.sleep(0.5)
+        print("Pedra - 1")
+        print(" ")
+        time.sleep(0.5)
+        print("Papel - 2")
+        print(" ")
+        time.sleep(0.5)
+        print("Tesoura - 3")
+        print(" ")
+        time.sleep(2)
+        
+        escolhaComputadorUm = random.randint(1, 3)
+
+        print("COMPUTADOR 1 FEZ SUA ESCOLHA")
+        print(divisao)
+        time.sleep(0.5)
+
+        print("COMPUTADOR 2 ESTÁ ESCOLHENDO UM ENTRE ESSES")
+        print(" ")
+        time.sleep(0.5)
+        print("Pedra - 1")
+        print(" ")
+        time.sleep(0.5)
+        print("Papel - 2")
+        print(" ")
+        time.sleep(0.5)
+        print("Tesoura - 3")
+        print(" ")
+        time.sleep(2)
+
+        escolhaComputadorDois = random.randint(1, 3)
+        print("COMPUTADOR 2 FEZ SUA ESCOLHA")
+
+        print(divisao)
+        time.sleep(1)
+        print(" ")
+        print(maoTres)
+        time.sleep(1)
+        print(" ")
+        print(maoDois)
+        time.sleep(1)
+        print(" ")
+        print(maoUm)
+        time.sleep(1)
+        print(" ")
+        print(ja)
+        time.sleep(1)
+        print(" ")
+        print(divisao)
+
+        if(escolhaComputadorUm == escolhaComputadorDois):
+            print("Empate!")
+
+        elif(escolhaComputadorUm == 1):
+            print("ESCOLHA COMPUTADOR UM:")
+            time.sleep(1)
+            print(pedra)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA COMPUTADOR DOIS:")
+            time.sleep(1)
+            if(escolhaComputadorDois == 2):
+                print(papel)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR DOIS venceu!")
+                placarComputadorDois+= 1
+            elif(escolhaComputadorDois == 3):
+                print(tesoura)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR UM venceu!")
+                placarComputadorUm+= 1
+
+        elif(escolhaComputadorUm == 2):
+            print("ESCOLHA COMPUTADOR UM:")
+            time.sleep(1)
+            print(papel)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA COMPUTADOR DOIS:")
+            time.sleep(1)
+            if(escolhaComputadorDois == 1):
+                print(pedra)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR UM venceu!")
+                placarComputadorUm+= 1
+            elif(escolhaComputadorDois == 3):
+                print(tesoura)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR DOIS venceu!")
+                placarComputadorDois+= 1
+
+        elif(escolhaComputadorUm == 3):
+            print("ESCOLHA COMPUTADOR UM:")
+            time.sleep(1)
+            print(tesoura)
+            print(" ")
+            time.sleep(1)
+            print(vs)
+            time.sleep(1)
+            print(" ")
+            print("ESCOLHA COMPUTADOR DOIS:")
+            time.sleep(1)
+            if(escolhaComputadorDois == 1):
+                print(pedra)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR DOIS venceu!")
+                placarComputadorDois+= 1
+            elif(escolhaComputadorDois == 2):
+                print(papel)
+                time.sleep(2)
+                print(divisao)
+                print("COMPUTADOR UM venceu!")
+                placarComputadorUm+= 1
+        
+        print(divisao)
+        print("Continuar - 1")
+        print(" ")
+        print("Sair - 0")
+        print(" ")
+        escolhaContinuar = int(input("Sua escolha: "))
+
+        while escolhaContinuar < 0 or escolhaContinuar > 1:
+          print(" ")
+          print("Escolha um número entre 0 e 1!")
+          print(" ")
+          escolha = int(input("Digite novamente sua escolha: "))
+        
+        if(escolhaContinuar == 0):
+            print(divisao)
+            time.sleep(1)
+            print(placar)
+            time.sleep(1)
+            print(" ")
+            print(f"COMPUTADOR UM - {placarComputadorUm} X COMPUTADOR DOIS - {placarComputadorDois}")
+            print(" ")
+            infinito = 0
